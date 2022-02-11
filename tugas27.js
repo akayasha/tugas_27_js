57cce53adb6c6930cb71821405e4850df9c2fd56
Firman Ismail <akayasha99@gmail.com>
2022-02-11 22:28:18 +0700

    tugas 27 js

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+let x = "NO";
+let y = " rumah saya";
+let z = 30;
+let a = "A";
+let b = "Jalan pangeran Tirtayasa No";
+let c = 70
+
+d = String(z)
+e = String(c)
+hasil1 =x+y+" "+d+a;
+hasil2 = b+" "+e
+console.log(hasil1);
+console.log(hasil2);
